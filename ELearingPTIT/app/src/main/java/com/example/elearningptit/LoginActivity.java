92ce3d203d92674744aba5838ade0d13c2b80fd0
huynhphuocsang <huynhphuocsang23@gmail.com>
2022-03-29 22:39:59 +0700

    login with jwt token

--- ELearingPTIT/app/src/main/java/com/example/elearningptit/LoginActivity.java
+++ ELearingPTIT/app/src/main/java/com/example/elearningptit/LoginActivity.java
@@ -6,10 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.elearningptit.model.JwtResponse;
+import com.example.elearningptit.model.LoginRequest;
+import com.example.elearningptit.remote.APICall;
+import com.example.elearningptit.tokenManager.TokenManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText txtUsername, txtPassword;
     Button btnLogin;
     boolean checkLoginSuccess = false;
+    TokenManager tokenManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +35,43 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkLoginSuccess = true;
-                finish();
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(intent);
+                final String username = txtUsername.getText().toString();
+                final String password = txtPassword.getText().toString();
+                LoginRequest loginRequest = new LoginRequest(username,password);
+                Call<JwtResponse> jwtResponseCall = APICall.apiCall.userLogin(loginRequest);
+
+                jwtResponseCall.enqueue(new Callback<JwtResponse>() {
+                    @Override
+                    public void onResponse(Call<JwtResponse> call, Response<JwtResponse> response) {
+                        if(response.code()==200){
+                            JwtResponse responseToken = response.body();
+                            tokenManager.createSession(username,responseToken.getToken());
+                            finish();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }else if(response.code()==401){
+                            showToast("Tên đăng nhập hoặc mật khẩu không đúng!");
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<JwtResponse> call, Throwable t) {
+                        showToast(t.toString());
+                    }
+                });
+//                checkLoginSuccess = true;
             }
         });
     }
 
     private void addControl() {
+        tokenManager = new TokenManager(getApplicationContext());
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
         btnLogin = findViewById(R.id.btnLogin);
+    }
+    private void showToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ ELearingPTIT/app/src/main/java/com/example/elearningptit/tokenManager/TokenManager.java
@@ -0,0 +1,27 @@
+package com.example.elearningptit.tokenManager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class TokenManager {
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private int mode = 0;
+    private static final String REFNAME = "JWTTOKEN";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_JWT_TOKEN = "jwttoken";
+    private Context context;
+
+
+    public TokenManager(Context context) {
+        this.context = context;
+        this.sharedPreferences = context.getSharedPreferences(REFNAME, mode);
+        this.editor = sharedPreferences.edit();
+    }
+
+    public void createSession(String username, String jwtValue) {
+        editor.putString(KEY_USERNAME, username);
+        editor.putString(KEY_JWT_TOKEN, jwtValue);
+        editor.commit();
+    }
+}
