3463fc53daa21402c18582489021f72123ef2f16
huynhphuocsang <huynhphuocsang23@gmail.com>
2022-03-30 21:19:06 +0700

    check login - save login and app icon

--- ELearingPTIT/app/src/main/java/com/example/elearningptit/MainActivity.java
+++ ELearingPTIT/app/src/main/java/com/example/elearningptit/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.elearningptit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.elearningptit.dialog.LogoutAlertDialogFragment;
+import com.example.elearningptit.tokenManager.TokenManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String REFNAME = "JWTTOKEN";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_JWT_TOKEN = "jwttoken";
+    private static final String IS_LOGIN = "login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +36,25 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    public void logoutAccount(View view) {
+        showDialog();
+    }
+    void showDialog() {
+        DialogFragment newFragment = LogoutAlertDialogFragment.newInstance();
+        newFragment.show(getSupportFragmentManager(), "dialog");
+    }
+
+    public void doPositiveClick() {
+        SharedPreferences preferences = getSharedPreferences(REFNAME,0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("login","false");
+        editor.apply();
+        finish();
+    }
+
+    public void doNegativeClick() {
+        Toast.makeText(this,"Thao tác bị hủy",Toast.LENGTH_SHORT).show();
     }
 }
