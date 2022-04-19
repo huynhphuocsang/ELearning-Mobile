e6f0c61b29dd3a5d40c20b4d6ddd8fb28776b70c
huynhphuocsang <huynhphuocsang23@gmail.com>
2022-04-19 09:17:46 +0700

    add menu for credit class

---
+++ ELearingPTIT/app/src/main/java/com/example/elearningptit/homeFragment/home_fragment.java
@@ -0,0 +1,94 @@
+package com.example.elearningptit.homeFragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.elearningptit.CreditClassActivity;
+import com.example.elearningptit.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link home_fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class home_fragment extends Fragment {
+
+    Button btnTemp ;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public home_fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment home_fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static home_fragment newInstance(String param1, String param2) {
+        home_fragment fragment = new home_fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home_fragment, container, false);
+        addControl(view);
+        setEvent();
+        return view;
+    }
+
+    private void setEvent() {
+        btnTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               try{
+                   Intent intent = new Intent(getActivity(), CreditClassActivity.class);
+                   startActivity(intent);
+               }catch (Exception e){
+                   Log.d("print",e.getMessage());
+               }
+
+            }
+        });
+    }
+
+    private void addControl(View view) {
+
+        btnTemp = view.findViewById(R.id.tempButton);
+    }
+}
