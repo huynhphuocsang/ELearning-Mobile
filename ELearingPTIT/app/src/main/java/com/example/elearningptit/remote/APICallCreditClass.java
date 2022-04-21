5418a1c27f6b502596b0306217755e72e0397385
huynhphuocsang <huynhphuocsang23@gmail.com>
2022-04-21 17:12:23 +0700

    add list view and credit class fragment

---
+++ ELearingPTIT/app/src/main/java/com/example/elearningptit/remote/APICallCreditClass.java
@@ -0,0 +1,26 @@
+package com.example.elearningptit.remote;
+
+import com.example.elearningptit.model.CreditClass;
+import com.example.elearningptit.model.Department;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
+public interface APICallCreditClass {
+    String BASE_URL = "http://192.168.1.13:8080/api/";
+
+    Gson gson = new GsonBuilder().create();
+    APICallCreditClass apiCall = new Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson)).build().create(APICallCreditClass.class);
+
+    @GET("credit-class/{pageNo}")
+    Call<List<CreditClass>> getCreditClass(@Header("Authorization") String token, @Path("pageNo") int pageNo);
+}
--- ELearingPTIT/app/src/main/java/com/example/elearningptit/remote/APICallSchoolYear.java
+++ ELearingPTIT/app/src/main/java/com/example/elearningptit/remote/APICallSchoolYear.java
@@ -2,9 +2,10 @@ package com.example.elearningptit.remote;
 
 import com.example.elearningptit.model.JwtResponse;
 import com.example.elearningptit.model.LoginRequest;
-import com.example.elearningptit.model.UserInfo;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
@@ -14,14 +15,13 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 
-public interface APICallSignin {
-     String BASE_URL = "http://192.168.1.13:8080/api/";
+public interface APICallSchoolYear {
+    String BASE_URL = "http://192.168.1.13:8080/api/";
 
     Gson gson = new GsonBuilder().create();
-    APICallSignin apiCall = new Retrofit.Builder().baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson)).build().create(APICallSignin.class);
+    APICallSchoolYear apiCall = new Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson)).build().create(APICallSchoolYear.class);
 
-    @POST("auth/signin")
-    Call<JwtResponse> userLogin(@Body LoginRequest loginRequest);
-
+    @GET("school-year/get-all")
+    Call<List<String>> getAllSchoolYear(@Header("Authorization") String token);
 }
