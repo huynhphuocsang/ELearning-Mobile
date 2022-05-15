47a6c688e2b38374252894e61ba9929cb925297f
Kevin <hiimvyxinh@gmail.com>
2022-05-16 00:51:36 +0700

    Vu fix some tiny bugs

--- ELearingPTIT/app/src/main/java/com/example/elearningptit/config/GlobalVariables.java
+++ ELearingPTIT/app/src/main/java/com/example/elearningptit/config/GlobalVariables.java
@@ -6,8 +6,8 @@ public class GlobalVariables {
 //    public static String BASE_URL="http://192.168.7.107:8080/api/";
 
     public static String BASE_URL="http://192.168.1.8:8080/api/";
-//    public static String BASE_URL="http://192.168.7.106:8080/api/";
-//    public static String BASE_URL="http://192.168.1.9:8080/api/";
+//    public static String BASE_URL="http://192.168.7.105:8080/api/";
+//    public static String BASE_URL="http://192.168.42.35:8080/api/";
 //    public static String BASE_URL="http://192.168.1.11:8080/api/";
 
 }
